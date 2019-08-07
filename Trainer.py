f768c1aa2ab8bf5c6f0eaccd97bbe75c2e571a52
Parimal Mehta <prmehta24@gmail.com>
2019-08-07 13:43:12 -0700

    add comments to all files made by me

--- Trainer.py
+++ Trainer.py
@@ -1,11 +1,14 @@
+#to encode a csv file such that it can be used to train a model
 from keyword_encode import encode_keywords
 import ray
 
 ray.init(object_store_memory=100 * 1000000,
          redis_max_memory=100 * 1000000)
 
-encode_keywords(csv_path='example/output.csv',
-                out_path='example/encodedStories.txt',
-                title_field='storytitle',
-                body_field='Story',
-                keyword_gen='Story')  
+encode_keywords(csv_path='example/output.csv',#path to input csv file
+                out_path='example/encodedStories.txt',#path for output file
+                title_field='storytitle',#column name for title column (optional)
+                body_field='Story',#column name for column with large amount of text depending on title field(optional)
+                keyword_gen='Story'#column name from which text keywords should be generated
+                #there are also category_field for category column if present | and keywords_field for a column filled with keywords for each field in keyword_gen column - if you don't want the model to extract keywords by itself 
+                )  
---
+++ example/gentxt.py
@@ -0,0 +1,13 @@
+#program to train gpt 2 on given encoded txt file
+import gpt_2_simple as gpt2
+
+model_name = "117M"
+gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/117M/
+
+sess = gpt2.start_tf_sess()
+gpt2.finetune(sess,
+              'encodedShakespeare.txt',# file path to file on which model is trained
+              model_name=model_name,
+              steps=1000)   # steps is max number of training steps
+
+gpt2.generate(sess) # generate text
