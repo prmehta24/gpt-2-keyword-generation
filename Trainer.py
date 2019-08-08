c7b7e5bdffc5a9263d8f1eaba6477b4351429045
Parimal Mehta <prmehta24@gmail.com>
2019-08-08 14:25:48 -0700

    Added more reference links for Trainer.py (Documentation)

--- Trainer.py
+++ Trainer.py
@@ -1,16 +1,17 @@
-#to encode a csv file such that it can be used to train a model
+#to encode a csv file such that it can be used to train a model/ To generate keywords for a csv file
 from keyword_encode import encode_keywords
 import ray
 
 ray.init(object_store_memory=100 * 1000000,
          redis_max_memory=100 * 1000000)
 
-#Reference: https://github.com/minimaxir/gpt-2-keyword-generation/tree/master/example 
+#Reference: https://github.com/minimaxir/gpt-2-keyword-generation/tree/master/example (Official Example of Keyword Generation)
 #OR
-#https://github.com/minimaxir/gpt-2-keyword-generation/blob/master/keyword_encode.py (Line 25)
-#for options for below function
+#https://github.com/minimaxir/gpt-2-keyword-generation (Taxonomy, Usage) - explains various parameters of encode_keyword()
+#OR
+#https://github.com/minimaxir/gpt-2-keyword-generation/blob/master/keyword_encode.py (Line 25)(encode_keywords function) - Displays all parameters and their default values for encode_keywords()
 
-encode_keywords(csv_path='example/output.csv',#path to input csv file
+encode_keywords(csv_path='example/ROCStories.csv',#path to input csv file
                 out_path='example/encodedStories.txt',#path for output file (must be .txt)
                 title_field='storytitle',#column name for title column (optional)
                 body_field='Story',#column name for column with large amount of text depending on title field(optional)
